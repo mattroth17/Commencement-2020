e2da330975f25b2083c2837edcb42df7f3ea7fce
matt roth <matthew.c.roth.22@dartmouth.edu>
2020-05-25 16:12:05 -0500

    added article titles/images/authors/links

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -15,7 +15,11 @@ function Square(props) {
 
 function Article(props) {
   return (
-    <div className='article'>{props.title}</div>
+    <button className='article' onClick={props.onClick}>
+      <img className='article_image' src={props.imageURL} />
+      <div className='article_title'>{props.title}</div>
+      <div className='article_author'>{props.author}</div>
+    </button>
   );
 }
 
@@ -76,22 +80,66 @@ class Articles extends React.Component {
     super(props);
     this.state = {
       articles: [
-        {title: 'article 1', URL: 'google.com', imageURL: './class_of_2020.jpg', author: 'jack', previewText: 'dnajndjandjsnjnaskjajsbdajbdajbdhbsdhbshdbhb'}, 
-        {title: 'article 2', URL: 'google.com', imageURL: './class_of_2020.jpg', author: 'jack'}, 
-        {title: 'article 3', URL: 'google.com', imageURL: './class_of_2020.jpg', author: 'jack'}, 
-        {title: 'article 4', URL: 'google.com', imageURL: './class_of_2020.jpg', author: 'jack'}, 
-        {title: 'article 5', URL: 'google.com', imageURL: './class_of_2020.jpg', author: 'jack'}, 
-        {title: 'article 6', URL: 'google.com', imageURL: './class_of_2020.jpg', author: 'jack'}, 
+        {
+          title: 'Founder of Khan Academy Salman Khan to speak at virtual commencement', 
+          URL: 'https://www.thedartmouth.com/article/2020/05/founder-of-khan-academy-salman-khan-to-speak-at-virtual-commencement', 
+          imageURL: 'https://snworksceo.imgix.net/drt/c4546441-fd5f-4798-ba71-0bffc946729f.sized-1000x1000.jpg?w=1000', 
+          author: 'The Dartmouth Senior Staff', 
+          abstract: ''
+        }, 
+        {
+          title: 'Amid Dartmouth’s uncertainty, peer schools offer early plans for fall term', 
+          URL: 'https://www.thedartmouth.com/article/2020/05/amid-dartmouths-uncertainty-peer-schools-offer-early-plans-for-fall-term', 
+          imageURL: 'https://snworksceo.imgix.net/drt/d3042f60-7e7d-4c71-b6f0-d3a4ed3d566b.sized-1000x1000.jpg?w=1000', 
+          author: 'Andrew Sasser', 
+          abstract: ''
+        }, 
+        {
+          title: 'Dick’s House counseling navigates move to teletherapy', 
+          URL: 'https://www.thedartmouth.com/article/2020/05/dicks-house-counseling-navigates-move-to-teletherapy', 
+          imageURL: 'https://snworksceo.imgix.net/drt/4de71614-1b81-4f5c-8be4-63aa8d7030fc.sized-1000x1000.jpg?w=1000', 
+          author: 'Aleka Kroitzsh', 
+          abstract: ''
+        }, 
+        {
+          title: '"Pine Pals” connects students and elderly Upper Valley residents though letters', 
+          URL: 'https://www.thedartmouth.com/article/2020/05/pine-pals-connects-students-and-elderly-upper-valley-residents-though-letters', 
+          imageURL: 'https://snworksceo.imgix.net/drt/81e4f4ab-01f4-48fa-84cb-ba6a3ae3bd1a.sized-1000x1000.jpg?w=1000', 
+          author: 'Emily Lu', 
+          abstract: ''}, 
+        {
+          title: 'With three representatives retiring, state Democrats plan for November', 
+          URL: 'https://www.thedartmouth.com/article/2020/05/with-three-representatives-retiring-state-democrats-plan-for-november', 
+          imageURL: 'https://snworksceo.imgix.net/drt/78d7a983-8db0-49b6-8fb3-e18a7e633f67.sized-1000x1000.jpg?w=1000', 
+          author: 'Sam Ferrone', 
+          abstract: ''
+        }, 
+        {
+          title: 'Stony Brook professor Jason Barabas ’93 named director of Rockefeller Center', 
+          URL: 'https://www.thedartmouth.com/article/2020/05/stony-brook-professor-jason-barabas-93-named-director-of-rockefeller-center', 
+          imageURL: 'https://snworksceo.imgix.net/drt/99f11936-ac91-4d88-aa4c-faf91e340d91.sized-1000x1000.jpg?w=1000', 
+          author: 'Emily Lu', 
+          abstract: ''
+        }, 
       
       ],
       
     }
   }
 
+  handleClick(url) {
+    window.open(url); 
+  }
+
   renderArticle(i) {
     return ( 
       <Article 
         title={this.state.articles[i].title} 
+        URL={this.state.articles[i].URL}
+        onClick = {() => this.handleClick(this.state.articles[i].URL)}
+        imageURL={this.state.articles[i].imageURL}
+        author={this.state.articles[i].author}
+        abstract={this.state.articles[i].abstract}
       />
     ); 
   }
