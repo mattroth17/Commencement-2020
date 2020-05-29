9bee127f05a7a873d4f25b24c9be5014f1d1f658
Zander Hirsch <zander@Zanders-MacBook-Air.local>
2020-05-29 12:42:08 -0400

    hover updated w/ text

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -13,6 +13,16 @@ function Article(props) {
       <img className='article_image' src={props.imageURL} />
       <div className='article_title'>{props.title}</div>
       <div className='article_author'>{props.author}</div>
+    </button>
+  );
+}
+
+function Abstract(props) {
+  return (
+    <button className='article' 
+    onClick={props.onClick}
+    onMouseEnter = {props.onMouseEnter}
+    onMouseLeave = {props.onMouseLeave}>
       <div className='article_abstract'>{props.abstract}</div>
     </button>
   );
@@ -81,7 +91,7 @@ class Articles extends React.Component {
           URL: 'https://www.thedartmouth.com/article/2020/05/founder-of-khan-academy-salman-khan-to-speak-at-virtual-commencement', 
           imageURL: 'https://snworksceo.imgix.net/drt/c4546441-fd5f-4798-ba71-0bffc946729f.sized-1000x1000.jpg?w=1000', 
           author: 'The Dartmouth Senior Staff', 
-          abstract: 'preview'
+          abstract: 'This year\u2019s virtual commencement speaker will be Salman Khan, founder of the free online education platform Khan Academy, Dartmouth announced on Thursday. The College selected Khan as commencement speaker before the switch to remote learning.'
         }, 
         {
           hasMouse: 'no',
@@ -89,7 +99,7 @@ class Articles extends React.Component {
           URL: 'https://www.thedartmouth.com/article/2020/05/amid-dartmouths-uncertainty-peer-schools-offer-early-plans-for-fall-term', 
           imageURL: 'https://snworksceo.imgix.net/drt/d3042f60-7e7d-4c71-b6f0-d3a4ed3d566b.sized-1000x1000.jpg?w=1000', 
           author: 'Andrew Sasser', 
-          abstract: 'preview'
+          abstract: 'As Dartmouth students finish their virtual spring terms, many are looking to other colleges for information about how and if they will conduct their fall terms. While the College has yet to offer specific guidance on the fall, College President Phil Hanlon and Provost Joseph Helble announced on May 4 that a final decision regarding fall term will be made by June 29.'
         }, 
         {
           hasMouse: 'no',
@@ -97,7 +107,7 @@ class Articles extends React.Component {
           URL: 'https://www.thedartmouth.com/article/2020/05/dicks-house-counseling-navigates-move-to-teletherapy', 
           imageURL: 'https://snworksceo.imgix.net/drt/4de71614-1b81-4f5c-8be4-63aa8d7030fc.sized-1000x1000.jpg?w=1000', 
           author: 'Aleka Kroitzsh', 
-          abstract: 'preview'
+          abstract: 'For the first month of spring term, Dick\u2019s House was unable to offer counseling services to students outside of New Hampshire due to licensing restrictions that prevented counselors from practicing out of state. After state and regional health boards loosened restrictions in light of COVID-19, the Counseling Center has explored new ways to connect with students as the College moves into its second term of remote learning.'
         }, 
         {
           hasMouse: 'no',
@@ -105,7 +115,7 @@ class Articles extends React.Component {
           URL: 'https://www.thedartmouth.com/article/2020/05/pine-pals-connects-students-and-elderly-upper-valley-residents-though-letters', 
           imageURL: 'https://snworksceo.imgix.net/drt/81e4f4ab-01f4-48fa-84cb-ba6a3ae3bd1a.sized-1000x1000.jpg?w=1000', 
           author: 'Emily Lu', 
-          abstract: 'preview'
+          abstract: 'To facilitate social connection during the COVID-19 pandemic, Dartmouth students and faculty members have launched a letter-writing program called Pine Pals, connecting students and elderly people in New Hampshire and Vermont.'
         }, 
         {
           hasMouse: 'no',
@@ -113,7 +123,7 @@ class Articles extends React.Component {
           URL: 'https://www.thedartmouth.com/article/2020/05/with-three-representatives-retiring-state-democrats-plan-for-november', 
           imageURL: 'https://snworksceo.imgix.net/drt/78d7a983-8db0-49b6-8fb3-e18a7e633f67.sized-1000x1000.jpg?w=1000', 
           author: 'Sam Ferrone', 
-          abstract: 'preview'
+          abstract: 'With three of Grafton County District 12\u2019s four representatives not seeking reelection, state representatives and organizers remain confident that Hanover will stay blue on Nov. 3, despite the pandemic\u2019s possible impact on student voter turnout.'
         }, 
         {
           hasMouse: 'no',
@@ -121,7 +131,7 @@ class Articles extends React.Component {
           URL: 'https://www.thedartmouth.com/article/2020/05/stony-brook-professor-jason-barabas-93-named-director-of-rockefeller-center', 
           imageURL: 'https://snworksceo.imgix.net/drt/99f11936-ac91-4d88-aa4c-faf91e340d91.sized-1000x1000.jpg?w=1000', 
           author: 'Emily Lu', 
-          abstract: 'preview'
+          abstract: 'Following a year-long international search, the College announced on Wednesday that Stony Brook University political science professor Jason Barabas \u201993 has been appointed director of the Rockefeller Center for Public Policy. Barabas currently directs the master\u2019s program in public policy at Stony Brook and will begin his position at Dartmouth, where he will also teach as a government professor, on July 1.'
         }, 
       
       ],
@@ -163,7 +173,7 @@ class Articles extends React.Component {
     }
     else{
       return(
-        <Article
+        <Abstract
           onMouseEnter = {() => this.handleMouse(i)}
           onMouseLeave = {() => this.handleLeave(i)}
           abstract={this.state.articles[i].abstract}
